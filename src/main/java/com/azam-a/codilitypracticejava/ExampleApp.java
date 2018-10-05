9db747965c78a8cf2c321f7f3716472e4d67b01b
azam alias <azamalias@gmail.com>
2018-10-05 22:52:30 +0800

    Add example Maven project with Travis CI config

---
+++ src/main/java/com/azam-a/codilitypracticejava/ExampleApp.java
@@ -0,0 +1,13 @@
+package com.azam_a.codilitypracticejava;
+
+/**
+ * Hello world!
+ *
+ */
+public class ExampleApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
